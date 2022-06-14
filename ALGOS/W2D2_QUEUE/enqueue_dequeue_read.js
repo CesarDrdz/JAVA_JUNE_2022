028fca101699ba29354d6b8110477509b0f8b4e4
jupiter <jupiterorbita@users.noreply.github.com>
2022-06-14 09:53:37 -0700

    add w2d2 queue enqueue, dequeue, peek, read

--- ALGOS/W2D2_QUEUE/enqueue_dequeue_read.js
+++ ALGOS/W2D2_QUEUE/enqueue_dequeue_read.js
@@ -21,21 +21,50 @@ class Queue {
     }
 
     // add nodes to the back of the queue
-    enqueue(node) { }
+    enqueue(node) {
+        if (this.back === null) { // if back is null, list is empty
+            this.back = node;
+            this.front = node;
+        } else { // otherwise add to back
+            this.back.next = node;
+            this.back = node;
+        }
+        this.length++;
+    }
 
     // remove from the front
-    dequeue() { }
+    dequeue() {
+        if (this.front === null) {
+            return null; // if empty return nothing
+        };
+        if (this.front === this.back) {
+            this.back = null;
+        };
+        let node = this.front;
+        this.front = node.next;
+        node.next = null;
+        this.length--;
+        return node;
+    }
 
     // check the front of the queue
-    peek() { }
+    peek() {
+        return this.front ? this.front.data : this.front;
+
+        if (this.front) {
+            return this.front.data
+        } else {
+            return this.front
+        }
+    }
 
     // return true / false if queue is empty
     isEmpty() {
         return this.front === null
     }
 
-    // return length
-    count() { }
+    // // return length
+    // count() { }
 }
 
 // NINJA BONUS:
@@ -44,4 +73,19 @@ class Queue {
 // return the queue back to it's original order when you are done
 // you are not allowed to linearly traverse the queue
 // only use public methods enqueue(), dequeue(), peek(), isEmpty(), and count()
-function readQueue(queue) { }
+function readQueue(queue) {
+    var tempQueue = new Queue();
+
+    while (!queue.isEmpty()) {
+        // 0x017erFd214
+        var tempNode = queue.dequeue();
+        console.log(tempNode.data);
+        tempQueue.enqueue(tempNode);
+    }
+
+    while (!tempQueue.isEmpty()) {
+        queue.enqueue(tempQueue.dequeue());
+    }
+
+    return queue;
+}
