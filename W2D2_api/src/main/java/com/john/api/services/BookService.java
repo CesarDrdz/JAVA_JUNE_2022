b5a68d5628822a6d4842256f88ee508407b98ed9
jupiter <jupiterorbita@users.noreply.github.com>
2022-06-14 14:05:28 -0700

    add w2d2 server MVC API @RestController

---
+++ W2D2_api/src/main/java/com/john/api/services/BookService.java
@@ -0,0 +1,42 @@
+package com.john.api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.john.api.models.Book;
+import com.john.api.repo.BookRepository;
+
+@Service
+public class BookService {
+	// adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    
+    
+    // READ ALL
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    
+    
+    // CREATE
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    
+    
+    // READ ONE
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+}
